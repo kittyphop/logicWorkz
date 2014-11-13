b531083259ac49acdb7b6272a8fcfa9c54a2d693
Kittiphon Phalakarn <kittyphon@gmail.com>
2014-11-13 15:26:25 +0700

    edit class Player and add class Gun, NormalGun, Probe

--- src/ConfigurableOption.java
+++ src/ConfigurableOption.java
@@ -1,22 +1,24 @@
 import java.awt.Dimension;
 
-
 public class ConfigurableOption {
-	private static int DIALOG_WIDTH  = 482;
+
+	private static int DIALOG_WIDTH = 482;
 	private static int DIALOG_HEIGHT = 411;
-	private static int WINDOW_WIDTH  = 800;
+	private static int WINDOW_WIDTH = 800;
 	private static int WINDOW_HEIGHT = 600;
-	
-	private static Dimension dialogDimension = new Dimension(DIALOG_WIDTH, DIALOG_HEIGHT);
-	private static Dimension windowDimension = new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT);
-	
-	public static Dimension getDialogDimension()
-	{
+	public static int MAX_TIME = 300;
+
+	private static Dimension dialogDimension = new Dimension(DIALOG_WIDTH,
+			DIALOG_HEIGHT);
+	private static Dimension windowDimension = new Dimension(WINDOW_WIDTH,
+			WINDOW_HEIGHT);
+
+	public static Dimension getDialogDimension() {
 		return dialogDimension;
 	}
-	
-	public static Dimension getWindowDimension()
-	{
+
+	public static Dimension getWindowDimension() {
 		return windowDimension;
 	}
+
 }
