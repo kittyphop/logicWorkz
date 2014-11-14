d767c61f2ff2a35063d8b8c352e5fe3a7879f4af
kittyphop <kittyphop@gmail.com>
2014-11-14 16:04:56 +0700

    edit set look and feel

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	public static void main(String[] args) {
 		try{
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			//UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel"); 
 		}catch(Exception e){}
 		new GameWindow();
 	}
